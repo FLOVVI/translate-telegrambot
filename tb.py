678e7053ac80e2638ad82674a0a53f985bdd4f95
FLOVVI <77698147+FLOVVI@users.noreply.github.com>
2022-04-19 23:33:30 +0300

    Add files via upload

--- tb.py
+++ tb.py
@@ -2,7 +2,6 @@ import googletrans
 from googletrans import Translator
 import telebot
 from telebot import types
-
 class Language:
     lang = "en"
 l = Language()
@@ -12,17 +11,18 @@ def start(message, res=False):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("Русский")
     item2=types.KeyboardButton("English")
+    item3=types.KeyboardButton("Український")
     markup.add(item1)
+    markup.add(item3)
     markup.add(item2)
+    
     bot.send_message(message.chat.id, f'Выберите на какой язык переводить:',  reply_markup=markup)
 @bot.message_handler(content_types=["ru"])
 def ru_leng(message):
     l.lang = "ru"
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     bot.send_message(message.chat.id, f'Ваш язык: Русский\nНапишите слово или предложение которое нужно перевести\n\nДля смены языка напишите /start',  reply_markup=markup)
-@bot.message_handler(content_types=["en"])
-def en_leng(message):
-    l.lang = "en"
+
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     if message.text.strip() == "ru" or message.text.strip() == "Русский":
@@ -37,7 +37,13 @@ def handle_text(message):
         item1=types.KeyboardButton("/start")
         markup.add(item1)
         bot.send_message(message.chat.id, f'Вы переводите на Английский язык \nНапишите слово или предложение которое нужно перевести\n\nДля смены языка напишите /start',  reply_markup=markup)
-    if message.text.strip() != "ru" and message.text.strip() != "en" and message.text.strip() != "English" and message.text.strip() != "Русский":    
+    if message.text.strip() == "uk" or message.text.strip() == "Український":
+        l.lang = "uk"
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("/start")
+        markup.add(item1)
+        bot.send_message(message.chat.id, f'Вы переводите на Украинский язык \nНапишите слово или предложение которое нужно перевести\n\nДля смены языка напишите /start',  reply_markup=markup)
+    if message.text.strip() != "ru" and message.text.strip() != "en" and message.text.strip() != "English" and message.text.strip() != "Русский" and message.text.strip() != "uk" and message.text.strip() != "Український":    
         translator = Translator()
         result = translator.translate(message.text, dest=l.lang)
         print(f"[{message.from_user.first_name}]", message.text)
@@ -46,5 +52,4 @@ def handle_text(message):
         item0=types.KeyboardButton("/start")
         markup.add(item0)
         bot.send_message(message.chat.id, f'[{l.lang}] {result.text}',  reply_markup=markup)
-
 bot.polling(none_stop=True, interval=0)                                         
