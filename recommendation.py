b459d34eaad20252c8038b071478f70b2bd61d69
FLOVVI <flovvi78@gmail.com>
2023-02-12 13:22:31 +0300

    Update

--- recommendation.py
+++ recommendation.py
@@ -24,3 +24,19 @@ def word_memorization(word, code):
             file.write(f'{word_en} ')
 
 
+class Word:
+    def __init__(self):
+        self.translate = Translate()
+
+    @staticmethod
+    def word_memorization(word, code):
+        # translate the word into English for writing
+        word_en = Translate().word_of_the_day_translate(word).title()
+        # checking for the existence of such a word
+        if wordnet.synsets(word_en):
+            with open(f'text_files/{code}', 'a', encoding='utf-8') as file:
+                file.write(f'{word_en} ')
+
+    @staticmethod
+    def request(self):
+        session = requests.Session()
