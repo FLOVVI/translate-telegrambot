609bb8abda10d719e1de167e085c3a572f78ba52
FLOVVI <77698147+FLOVVI@users.noreply.github.com>
2023-02-12 03:25:17 +0300

    Update

--- recommendation.py
+++ recommendation.py
@@ -18,6 +18,7 @@ def generate_code():
 def word_memorization(word, code):
     # translate the word into English for writing
     word_en = Translate().word_of_the_day_translate(word).title()
+    # checking for the existence of such a word
     if wordnet.synsets(word_en):
         with open(f'text_files/{code}', 'a', encoding='utf-8') as file:
             file.write(f'{word_en} ')
