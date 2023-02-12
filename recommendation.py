7dee40de9d3f187e12e5456fd1cbd8d07f20e8dc
FLOVVI <flovvi78@gmail.com>
2023-02-12 22:09:46 +0300

    connected the neural network

--- recommendation.py
+++ recommendation.py
@@ -1,11 +1,9 @@
 import random
 import sqlite3
-import requests
-from bs4 import BeautifulSoup as bs
+from config import openai_token
 from translator import Translate
 from nltk.corpus import wordnet
-
-URL = 'https://www.generatormix.com/synonym-generator'
+import openai
 
 
 def generate_code():
@@ -13,6 +11,7 @@ def generate_code():
     repeat = [i[0] for i in sqlite3.connect('translatebot.db').cursor().execute('SELECT code FROM tableone').fetchall()]
     while True:
         code = ''
+        # generate a four-digit code
         for i in range(4):
             code += random.choice(letters)
         if code not in repeat:
@@ -20,18 +19,13 @@ def generate_code():
     return code
 
 
-def word_memorization(word, code):
-    # translate the word into English for writing
-    word_en = Translate().word_of_the_day_translate(word).title()
-    # checking for the existence of such a word
-    if wordnet.synsets(word_en):
-        with open(f'text_files/{code}', 'a', encoding='utf-8') as file:
-            file.write(f'{word_en} ')
-
-
 class Word:
     def __init__(self):
         self.translate = Translate()
+        # session token
+        openai.api_key = openai_token
+        # chat 3 engine (max)
+        self.model_engine = "text-davinci-003"
 
     @staticmethod
     def word_memorization(word, code):
@@ -42,6 +36,13 @@ class Word:
             with open(f'text_files/{code}', 'a', encoding='utf-8') as file:
                 file.write(f'{word_en} ')
 
-    @staticmethod
-    def request():
-        session = requests.Session()
+    def get_similar(self, word):
+        completion = openai.Completion.create(
+            engine=self.model_engine,
+            prompt=f'write a no more than five words that are often used together with the word {word}',
+            max_tokens=3900 - len(word),
+            temperature=0.1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        return completion.choices[0].text
