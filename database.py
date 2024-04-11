ef5ad571134fbea27b32ed1dceb30518ad0fb01f
FLOVVI <flovvi78@gmail.com>
2024-04-11 12:28:15 +0300

    Bug fixes, optimization improvements

--- database.py
+++ database.py
@@ -1,9 +1,30 @@
 import sqlite3
-import random
-import time
+import random, time, os
+import yadisk
 from threading import Thread
-from database_cloud import DatabaseCloud
 from config import yadisk_token, server_usage
+
+
+class DatabaseCloud:
+
+    def __init__(self, token):
+        self.disk = yadisk.YaDisk(token=token)
+
+    def upload(self, user_file, disk_file, remove=True):
+        if disk_file in self.listdir() and remove:
+            self.remove(disk_file)
+        self.disk.upload(user_file, f'database/{disk_file}')
+
+    def download(self, user_file, disk_file):
+        os.remove(user_file)
+        self.disk.download(f'database/{disk_file}', user_file)
+
+    def listdir(self):
+        return [i.name for i in list(self.disk.listdir("/database"))]
+
+    def remove(self, disk_file):
+        self.disk.remove(f'database/{disk_file}')
+
 
 cloud = DatabaseCloud(yadisk_token)
 if server_usage:
@@ -145,6 +166,5 @@ class Database:
             if data.upload_timer:
                 th = Thread(target=upload)
                 th.start()
-
 
 Add().search_table()
